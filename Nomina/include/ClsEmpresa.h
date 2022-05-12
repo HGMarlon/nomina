319ba6169c181ee700b7d140263cc90da08ed03d
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-05-12 12:40:44 -0600

    creando clase empresa

---
+++ Nomina/include/ClsEmpresa.h
@@ -0,0 +1,16 @@
+#ifndef CLSEMPRESA_H
+#define CLSEMPRESA_H
+
+
+class ClsEmpresa
+{
+    public:
+        ClsEmpresa();
+        virtual ~ClsEmpresa();
+
+    protected:
+
+    private:
+};
+
+#endif // CLSEMPRESA_H
---
+++ Nomina/src/ClsEmpresa.cpp
@@ -0,0 +1,11 @@
+#include "ClsEmpresa.h"
+
+ClsEmpresa::ClsEmpresa()
+{
+    //ctor
+}
+
+ClsEmpresa::~ClsEmpresa()
+{
+    //dtor
+}
