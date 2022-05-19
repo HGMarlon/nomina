ac6c829cef64f14bb1012b6960d3c76283cd36b2
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-18 22:30:57 -0600

    metodos de los atributos

--- Planilla/include/Clsnomina.h
+++ Planilla/include/Clsnomina.h
@@ -57,6 +57,59 @@ class Clsnomina
         void mestablecerNombreENomina( string );
         string mobtenerNombreENomina() const;
 
+//Karla gómez 9959-21-1896
+        //Metodos atributo cargo del empleado
+        void mestablecerCargoNomina( string );
+        string mobtenerCargoNomina() const;
+
+        //Metodos atributo salario del empelado
+        void mestablecerSalarioNomina( float );
+        float mobtenerSalarioNomina() const;
+
+        //Metodos atributo dias laborados
+        void mestablecerDiasTrabNomina(float);
+        float mobtenerDiasTrabNomina() const;
+
+        //Metodos atributo sueldo ordinario
+        void mestablecerSueldoOrNomina(float);
+        float mobtenerSueldoOrNomina() const;
+
+        //Metodos atributo horas extra laboradas
+        void mestablecerHoraExtras(float);
+        float mobtenerHoraExtra()const;
+
+        //Metodos atributo sueldo extraordinario
+        void mestablecersueldoExtraNomina(float);
+        float mobtenersueldoExtraNomina()const;
+
+        //Metodos atributo bonificacion  incentivo
+        void mestablecerbonificacionincNomina(float);
+        float mobtenerbonificacionincNomina()const;
+
+        //Metodos atributo total devengado
+        void mestablecertotaldevenNomina(float);
+        float mobtenertotaldevenNomina()const;
+
+        //Metodos atributo descuento IGSS
+        void mestablecerIGGNomina( float );
+        float mobtenerIGGNomina() const;
+
+        //Metodos atributo descuento ISR
+        void mestablecerISRNomina( float );
+        float mobtenerISRNomina() const;
+
+        //Metodos atributo anticipos
+        void mestableceranticipoNomina( float );
+        float mobteneranticipoNomina() const;
+
+        //Metodos atributo total descuentos
+        void mestablecertotaldescuentNomina( float );
+        float mobtenertotaldescuentNomina() const;
+
+        //Metodos atributo liquido a recibir
+        void mestablecerliquidoNomina( float );
+        float mobtenerliquidoNomina() const;
+
         virtual ~Clsnomina();
 
     protected:
