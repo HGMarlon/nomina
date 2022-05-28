c81814c47d6495fde226712686ca09f516f847ce
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-27 23:39:53 -0600

    clase reporte agragada

---
+++ Planilla/include/ClsReporte.h
@@ -0,0 +1,25 @@
+//Karla Sofia Gómez Tobar 9959-21-1896
+#ifndef CLSREPORTE_H
+#define CLSREPORTE_H
+
+#include<conio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<cstdlib>
+
+using namespace std;
+
+class ClsReporte
+{
+    public:
+        ClsReporte();
+        virtual ~ClsReporte();
+
+    protected:
+
+    private:
+};
+
+#endif // CLSREPORTE_H
---
+++ Planilla/src/ClsReporte.cpp
@@ -0,0 +1,24 @@
+#include "ClsReporte.h"
+
+#include<conio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<cstdlib>
+#include<ctime>
+#include<string.h>
+
+
+using namespace std;
+//Karla Sofia Gómez Tobar 9959-21-1896
+
+ClsReporte::ClsReporte()
+{
+    //ctor
+}
+
+ClsReporte::~ClsReporte()
+{
+    //dtor
+}
