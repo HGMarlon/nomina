feb03942c8a471334b5284adb0f61b8a4a03cf55
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-27 23:46:34 -0600

    funciones de los atributos agregados

--- Planilla/src/ClsReporte.cpp
+++ Planilla/src/ClsReporte.cpp
@@ -23,6 +23,59 @@ ClsReporte::ClsReporte(int iClaveReporte, int iDiasTrabajados, int iHorasExtra, 
     mestablecerBonificacion(fAnticipo);
 }
 
+//Karla Sofia Gómez Tobar 9959-21-1896
+int ClsReporte::mobtenerClaveRepo() const
+{
+    return m_iClaveReporte;
+}
+
+void ClsReporte::mestablecerClaveRepo (int ienteroClave)
+{
+    m_iClaveReporte = ienteroClave;
+}
+
+int ClsReporte::mobtenerDiasTrab() const
+{
+    return m_iDiasTrabajados;
+}
+
+void ClsReporte::mestablecerDiasTrab (int ienteroDias)
+{
+    m_iDiasTrabajados = ienteroDias;
+}
+
+int ClsReporte::mobtenerHorasExtra() const
+{
+    return m_iHorasExtra;
+}
+
+void ClsReporte::mestablecerHorasExtra (int ienteroHoras)
+{
+    m_iHorasExtra = ienteroHoras;
+}
+
+
+float ClsReporte::mobtenerBonificacion() const
+{
+    return m_fBonificacion;
+}
+
+void ClsReporte::mestablecerBonificacion (float fflotanteBonificacion)
+{
+    m_fBonificacion = fflotanteBonificacion;
+}
+
+loat ClsReporte::mobtenerAnticipo() const
+{
+    return m_fAnticipo;
+}
+
+void ClsReporte::mestablecerAnticipo (float fflotanteAnticipo)
+{
+    m_fAnticipo = fflotanteAnticipo;
+}
+
+
 ClsReporte::~ClsReporte()
 {
     //dtor
