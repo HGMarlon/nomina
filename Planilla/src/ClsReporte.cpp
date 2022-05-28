7369339f4b970668fae9934a20e8ed62ebf72aef
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-27 23:45:13 -0600

    metodos de los atributos en cpp agregados

--- Planilla/src/ClsReporte.cpp
+++ Planilla/src/ClsReporte.cpp
@@ -13,9 +13,14 @@
 using namespace std;
 //Karla Sofia Gómez Tobar 9959-21-1896
 
-ClsReporte::ClsReporte()
+ClsReporte::ClsReporte(int iClaveReporte, int iDiasTrabajados, int iHorasExtra, float fBonificacion, float fAnticipo )
 {
     //ctor
+    mestablecerClaveRepo(iClaveReporte);
+    mestablecerDiasTrab(iDiasTrabajados);
+    mestablecerHorasExtra(iHorasExtra);
+    mestablecerBonificacion(fBonificacion);
+    mestablecerBonificacion(fAnticipo);
 }
 
 ClsReporte::~ClsReporte()
