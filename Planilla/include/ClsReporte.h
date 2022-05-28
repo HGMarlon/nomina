bcedf340b2a13d67886c2561623a3fd6cc8426ca
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-27 22:37:54 -0600

    clase reporte agregada

---
+++ Planilla/include/ClsReporte.h
@@ -0,0 +1,23 @@
+#ifndef CLSREPORTE_H
+#define CLSREPORTE_H
+
+#include<conio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<cstdlib>
+
+
+class ClsReporte
+{
+    public:
+        ClsReporte();
+        virtual ~ClsReporte();
+
+    protected:
+
+    private:
+};
+
+#endif // CLSREPORTE_H
---
+++ Planilla/src/ClsReporte.cpp
@@ -0,0 +1,20 @@
+#include "ClsReporte.h"
+
+#include<conio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<cstdlib>
+#include<ctime>
+#include<string.h>
+
+ClsReporte::ClsReporte()
+{
+    //ctor
+}
+
+ClsReporte::~ClsReporte()
+{
+    //dtor
+}
