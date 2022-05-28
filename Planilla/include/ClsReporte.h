3f83940daa574524e9d860a8f91ea4e431b6c359
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-27 23:41:36 -0600

    metodos agrgados al .h

--- Planilla/include/ClsReporte.h
+++ Planilla/include/ClsReporte.h
@@ -14,6 +14,31 @@ using namespace std;
 class ClsReporte
 {
     public:
+        ClsReporte(int=0, int=0, int=0, float=0, float=0 );
+
+        mmenuReporte();
+        mcrearReporte();
+
+        int mobtenerIndicadorRep( const char * const );
+
+        void mnuevoReporte(fstream &archivoReporte);
+
+        void mconsultarRegistroReporte(fstream &archivoReporte);
+
+        void mostrarLineaReporte( const ClsPuestos &registroReporte );
+
+        void mmodificarRegistroReporte( fstream &archivoReporte );
+
+        void mmostrarLineaRegistroReporte( ostream &salida, const ClsReporte &registroReporte );
+
+        void mimprimirRegistroReporte(fstream &archivoReporte);
+
+        void meliminarRegistroReporte(fstream &archivoReporte);
+
+        void mbuscarReporte(fstream &archivoReporte);
+
+
+
         ClsReporte();
         virtual ~ClsReporte();
 
