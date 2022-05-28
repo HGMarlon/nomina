3513f51360072fd0eeac277900f13bdbb819803a
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-05-27 22:45:26 -0600

    metodos declarados .h

--- Planilla/include/ClsReporte.h
+++ Planilla/include/ClsReporte.h
@@ -8,11 +8,35 @@
 #include<iomanip>
 #include<cstdlib>
 
+using namespace std;
 
 class ClsReporte
 {
     public:
-        ClsReporte();
+        ClsPuestos(int=0, int=0, int=0, float=0, float=0 );
+
+        mmenuReporte();
+        mcrearReporte();
+
+        int mobtenerIndicadorRep( const char * const );
+
+        void mnuevoReporte(fstream &archivoReporte);
+
+        void mconsultarRegistroReporte(fstream &archivoReporte);
+
+        void mostrarLineaReporte( const ClsPuestos &registroReporte );
+
+        void mmodificarRegistroReporte( fstream &archivoReporte );
+
+        void mmostrarLineaRegistroReporte( ostream &salida, const ClsReporte &registroReporte );
+
+        void mimprimirRegistroReporte(fstream &archivoReporte);
+
+        void meliminarRegistroReporte(fstream &archivoReporte);
+
+        void mbuscarReporte(fstream &archivoReporte);
+
+
         virtual ~ClsReporte();
 
     protected:
